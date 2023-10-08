64f0b98f0bca4452ac019748bb136edf003caba2
irinaBerendeeva87 <irina.berendeeva87@gmail.com>
2023-10-08 11:13:05 +0200

    Write Test 2

--- src/main/java/org/example/MainClass.java
+++ src/main/java/org/example/MainClass.java
@@ -1,9 +1,10 @@
 package org.example;
 
 public class MainClass {
+    private int class_number = 20;
 
-    public int getLocalNumber(){
-        return 14;
+    public int getClassNumber() {
+        return this.class_number;
     }
 
 }
--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -1,16 +1,15 @@
 import org.example.MainClass;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class MainClassTest {
     MainClass mainClass = new MainClass();
 
     @Test
-    public void testGetLocalNumber(){
-        int actual = mainClass.getLocalNumber();
-        int expected = 14;
-        assertEquals("The number is not equal to 14", expected, actual);
+    public void testGetClassNumber() {
+        boolean result = mainClass.getClassNumber() > 45;
+        assertTrue("Test failed. Expected value greater than 45", result);
     }
 
 }
