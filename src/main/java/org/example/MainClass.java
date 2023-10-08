655f20fbd8ab280f3e9cf8ca30bbd0469eb05202
irinaBerendeeva87 <irina.berendeeva87@gmail.com>
2023-10-08 09:46:25 +0200

    write Test 1

--- src/main/java/org/example/MainClass.java
+++ src/main/java/org/example/MainClass.java
@@ -5,4 +5,5 @@ public class MainClass {
     public int getLocalNumber(){
         return 14;
     }
+
 }
--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -3,12 +3,14 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class MainClassTest extends MainClass {
+public class MainClassTest {
+    MainClass mainClass = new MainClass();
 
     @Test
     public void testGetLocalNumber(){
-        int actual = super.getLocalNumber();
+        int actual = mainClass.getLocalNumber();
         int expected = 14;
         assertEquals("The number is not equal to 14", expected, actual);
     }
+
 }
