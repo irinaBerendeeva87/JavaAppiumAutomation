26a8ed3f36af46efc0d8c931b0abd6de7d1d9aea
irinaBerendeeva87 <irina.berendeeva87@gmail.com>
2023-10-18 13:45:26 +0200

    add first test with capabilities

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,36 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class FirstTest {
+    private AppiumDriver driver;
+
+    @Before
+    public void setUp()throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("appium:platformName","Android");
+        capabilities.setCapability("deviceName", "AndroidEmulator");
+        capabilities.setCapability("platformVersion", "9.0");
+        capabilities.setCapability("appium:automationName", "UiAutomator2");
+        capabilities.setCapability("appium:appPackage", "org.wikipedia");
+        capabilities.setCapability("appium:appActivity", ".main.MainActivity");
+        capabilities.setCapability("appium:app", "/Users/irina/Desktop/JavaAppiumAutomation/JavaAppiumAutomation/apks/org.wikipedia.apk");
+
+        driver = new AndroidDriver(new URL("http://0.0.0.0:4723/"),capabilities);
+    }
+
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+    public void firstTest(){
+        System.out.println("First test");
+    }
+}
