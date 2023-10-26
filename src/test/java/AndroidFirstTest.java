1a1959f5990e239ee22e49785c388f50fdc8ed3d
irinaBerendeeva87 <irina.berendeeva87@gmail.com>
2023-10-26 15:01:18 +0200

    add cancelSearch test

--- src/test/java/AndroidFirstTest.java
+++ src/test/java/AndroidFirstTest.java
@@ -8,8 +8,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.net.URL;
+import static org.junit.Assert.assertTrue;
 
 public class AndroidFirstTest {
     private AppiumDriver driver;
@@ -48,16 +48,42 @@ public class AndroidFirstTest {
                 5);
     }
 
+    @Test
+    public void cancelSearch(){
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/fragment_onboarding_skip_button"),
+                "Can't find the Skip bottom",
+                5);
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Can't find 'Search Wikipedia'",
+                5);
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text, 'Search')]"),"Netherlands",
+                "Can't find search input",
+                5 );
+        waitForElementPresent(By.className("androidx.recyclerview.widget.RecyclerView"),
+                "Search_results_list is empty",
+                5);
+        waitForElementAndClick(By.id("org.wikipedia:id/search_close_btn"),
+                "Can't find X on search field",
+                5);
+        boolean isElementPresent =  waitForElementNotPresent(By.id("org.wikipedia:id/search_results_list"),
+                "Results are still present on the page",
+                5);
+        assertTrue("Results are still present on the page",isElementPresent);
+    }
+
     private WebElement waitForElementAndClick(By locator, String error_message, long timeoutInSecond){
         WebElement element = waitForElementPresent(locator, error_message, timeoutInSecond);
         element.click();
         return element;
     }
 
-    private WebElement waitForElementPresent(By by, String error_message, long timeoutInSecond){
+    private WebElement waitForElementPresent(By locator, String error_message, long timeoutInSecond){
         WebDriverWait wait = new WebDriverWait(driver, timeoutInSecond);
         wait.withMessage(error_message + "\n");
-        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+        return wait.until(ExpectedConditions.presenceOfElementLocated(locator));
     }
 
     private void assertElementHasText(By locator,String expectedText, String error_message, long timeoutInSecond) {
@@ -66,6 +92,18 @@ public class AndroidFirstTest {
         if (!actualText.equals(expectedText)) {
             throw new AssertionError(error_message);
         }
+    }
+
+    private WebElement waitForElementAndSendKeys(By locator, String value, String error_message, long timeoutInSecond){
+        WebElement element = waitForElementPresent(locator, error_message,timeoutInSecond);
+        element.sendKeys(value);
+        return element;
+    }
+    private boolean waitForElementNotPresent(By locator, String error_message, long timeoutInSecond){
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSecond);
+        wait.withMessage(error_message + "\n");
+        return wait.until(
+                ExpectedConditions.invisibilityOfElementLocated(locator));
     }
 }
 
